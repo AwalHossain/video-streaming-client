2a6143d4303dada6e888050f72a1445e8f4a8a55
Awalhossain <waqin21@gmail.com>
2023-11-13 15:26:46 +0600

    feat: add loading & user state 🔃

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,16 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useCheckSessionQuery } from "../redux/features/auth/authApi";
 
-
-
 const useAuth = () => {
-    const { data: user, isLoading: loading } = useCheckSessionQuery();
+    const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState(null);
 
+    const { data, isLoading } = useCheckSessionQuery();
+    console.log('data', data);
     useEffect(() => {
+        if (data?.data) {
+            setUser(data?.data);
+            setLoading(false);
+        }
+        setLoading(isLoading);
+    }, [isLoading, data]);
 
-    }, [])
-
-    return { user, loading }
-}
+    return { user, loading };
+};
 
 export default useAuth;
