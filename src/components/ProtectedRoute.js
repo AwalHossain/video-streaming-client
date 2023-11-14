d4e977a8dbec5f2225ccfcdbf905b7077b68300e
Awalhossain <waqin21@gmail.com>
2023-11-14 09:39:46 +0600

    refactor: updated protected route code ➿

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,23 +1,24 @@
-
-
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
 
 const ProtectedRoute = ({ children }) => {
-
     const { user, loading } = useAuth();
-    const navigate = useNavigate();;
+    const navigate = useNavigate();
+
+    console.log('user', user, 'loading', loading);
+
+    useEffect(() => {
+        if (!user && !loading) {
+            navigate('/login');
+        }
+    }, [user, loading, navigate]);
 
     if (loading) {
         return <div>Loading...</div>
-    }
-    if (!user && !loading) {
-        navigate('/login');
-        return null;
     }
 
     return children;
 }
 
-export default ProtectedRoute
+export default ProtectedRoute;
