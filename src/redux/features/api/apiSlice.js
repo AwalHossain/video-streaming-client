69546a259bec0b40532f3c3cf795983923dbb7f5
Awalhossain <waqin21@gmail.com>
2023-11-14 09:41:55 +0600

    refactor: change baseQuery to use axiosBaseQuery

--- src/redux/features/api/apiSlice.js
+++ src/redux/features/api/apiSlice.js
@@ -1,13 +1,12 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi } from '@reduxjs/toolkit/query/react'
+import axiosBaseQuery from '../../../helpers/axios/axiosBaseQuery'
 
 console.log(process.env.REACT_APP_API_URL, 'check')
 export const apiSlice = createApi({
     name: 'api',
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({
-        baseUrl: process.env.REACT_APP_API_URL,
-        credentials: 'include'
-
+    baseQuery: axiosBaseQuery({
+        baseUrl: process.env.REACT_APP_API_URL
     }),
     endpoints: (builder) => ({}),
     tagTypes: []
