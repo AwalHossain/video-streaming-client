10827a99631e3f06ed1beec27b97f54f8c660f72
AwalHossain <waqin21@gmail.com>
2023-11-16 14:42:10 +0000

    refactor: modiefied initialState 🚀

--- src/redux/features/auth/authSlice.js
+++ src/redux/features/auth/authSlice.js
@@ -1,26 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit"
-
-
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    user: undefined,
+    user: {
+        name: 'Guest',
+        email: '',
+        picture: ''
+
+    },
 }
-
-
 
 const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers: {
         userLoggedIn: (state, action) => {
+            console.log('State before userLoggedIn:', state);
+            console.log('Action for userLoggedIn:', action);
             state.user = action.payload
+            console.log('State after userLoggedIn:', state);
         },
         userLoggedOut: (state) => {
+            console.log('State before userLoggedOut:', state);
             state.user = undefined
+            console.log('State after userLoggedOut:', state);
         }
     }
 })
-
 
 export const { userLoggedIn, userLoggedOut } = authSlice.actions
 
