ebda02b5db51d697645ba56f082e4c3f0c853195
AwalHossain <waqin21@gmail.com>
2023-11-20 04:49:50 +0000

    feat: Add socketSliceReducer to store 📝

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { apiSlice } from './features/api/apiSlice'
 import authSliceReducer from './features/auth/authSlice'
+import socketSliceReducer from './features/socket/socketSlice'
 
 export const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
-        auth: authSliceReducer
+        auth: authSliceReducer,
+        socket: socketSliceReducer,
 
     },
     // adding api middleware enables caching, invalidation, polling, and other features of `rtk-query`
