20c24ec5c7555d635b06c87dd09d4b160635bb8b
AwalHossain <waqin21@gmail.com>
2023-11-20 04:50:39 +0000

    🗺  Update API base URL

--- src/redux/features/api/apiSlice.js
+++ src/redux/features/api/apiSlice.js
@@ -1,12 +1,13 @@
 import { createApi } from '@reduxjs/toolkit/query/react'
 import axiosBaseQuery from '../../../helpers/axios/axiosBaseQuery'
+import REACT_APP_API_URL from '../../../utils/apiUrl'
 
-console.log(process.env.REACT_APP_API_URL, 'check')
+console.log(process.env.REACT_APP_BASE_URL, 'check')
 export const apiSlice = createApi({
     name: 'api',
     reducerPath: 'api',
     baseQuery: axiosBaseQuery({
-        baseUrl: process.env.REACT_APP_API_URL
+        baseUrl: REACT_APP_API_URL,
     }),
     endpoints: (builder) => ({}),
     tagTypes: []
