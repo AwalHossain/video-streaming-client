2c8626453b8950a9a841856f64cc0edb14e7ec1a
AwalHossain <waqin21@gmail.com>
2023-11-20 04:55:36 +0000

    🛠  refactor: NotificationBar component

--- src/utils/NotificationBar.js
+++ src/utils/NotificationBar.js
@@ -1,26 +1,17 @@
-
-
 import { Alert, Snackbar } from '@mui/material';
 import React from 'react';
 
 const NotificationBar = ({ state, setState, severity }) => {
-    console.log(state, 'state');
     return (
         <div>
             <Snackbar
                 open={!!state}
                 autoHideDuration={5000}
-                onClose={() => {
-                    setState(null);
-                }}
                 anchorOrigin={{ vertical: "top", horizontal: "center" }}
             >
                 <Alert
                     variant="filled"
-                    onClose={() => {
-                        setState(null);
-                    }}
-                    severity={severity}
+                    severity={severity || ""}
                 >
                     {state}
                 </Alert>
