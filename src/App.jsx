3033f8e7dd8c0731b05bfe793eef09f7b799c1b3
AwalHossain <waqin21@gmail.com>
2023-11-21 05:22:48 +0000

    refactor: remove unused code 🛠

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer, useState } from "react";
+import React, { useState } from "react";
 
 import { Alert, Snackbar, Stack } from "@mui/material";
 
@@ -8,41 +8,39 @@ import Router from "./routes";
 import ThemeProvider from "./theme";
 
 // components
-import { useSelector } from "react-redux";
-import ProgressModal from "./components/modal/ProgresModal";
+import ProgressModals from "./components/modal/ProgressModals";
 import ScrollToTop from "./components/scroll-to-top";
-import { useSubscribeToEventsQuery } from "./redux/features/socket/socketApi";
+import { ProgressProvider } from "./contexts/ProgressContext";
 // Modify your processReducer to handle actions
-const processReducer = (state, action) => {
-  switch (action.type) {
-    case "SET_PROCESS":
-      const { fileName, name, ...rest } = action.payload;
-      return {
-        ...state,
-        [fileName]: {
-          ...state[fileName],
-          [name]: { fileName, name, ...rest },
-        },
-      };
-    case "RESET_PROCESS":
-      return {};
-    default:
-      return state;
-  }
-};
+// const processReducer = (state, action) => {
+//   switch (action.type) {
+//     case "SET_PROCESS":
+//       const { fileName, name, ...rest } = action.payload;
+//       return {
+//         ...state,
+//         [fileName]: {
+//           ...state[fileName],
+//           [name]: { fileName, name, ...rest },
+//         },
+//       };
+//     case "RESET_PROCESS":
+//       return {};
+//     default:
+//       return state;
+//   }
+// };
 
 export default function App() {
-  useSubscribeToEventsQuery();
   const [wsResponse, setWsResponse] = useState(null);
-  // const [process, setProcess] = useState({});
-  const [process, dispatch] = useReducer(processReducer, {});
-  const data = useSelector((state) => state.socket);
-  // Modify your useEffect to dispatch an action with a type and payload
-  useEffect(() => {
-    dispatch({ type: "SET_PROCESS", payload: data.process });
-  }, [data]);
+  // // const [process, setProcess] = useState({});
+  // const [process, dispatch] = useReducer(processReducer, {});
+  // const data = useSelector((state) => state.socket);
+  // // Modify your useEffect to dispatch an action with a type and payload
+  // useEffect(() => {
+  //   dispatch({ type: "SET_PROCESS", payload: data.process });
+  // }, [data]);
 
-  console.log(process, "process rom app", data.process, "data process");
+  console.log("process", "process rom app", "data process");
 
   return (
     <ThemeProvider>
@@ -69,33 +67,9 @@ export default function App() {
             {wsResponse}
           </Alert>
         </Snackbar>
-        <Stack
-          direction="column"
-          justifyContent="flex-end"
-          alignItems="flex-end"
-          spacing={2}
-          sx={{
-            position: "fixed",
-            bottom: "20px",
-            right: "5px",
-            zIndex: 9999,
-          }}
-        >
-          {Object.entries(process).map(([videoId, videoProcess]) =>
-            Object.entries(videoProcess).map(
-              ([name, process]) =>
-                process.status === "processing" && (
-                  <ProgressModal
-                    key={`${videoId}-${name}`}
-                    name={name}
-                    fileName={process.fileName}
-                    status={process.status}
-                    progress={process.progress}
-                  />
-                )
-            )
-          )}
-        </Stack>
+        <ProgressProvider>
+          <ProgressModals />
+        </ProgressProvider>
       </Stack>
     </ThemeProvider>
   );
