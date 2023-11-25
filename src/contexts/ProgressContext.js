52691b6e7fadfe8478cbcf2d7f5522cc54bb101f
AwalHossain <waqin21@gmail.com>
2023-11-25 23:05:43 +0000

    feat: Add upload progress tracking to ProgressContext 🔄

--- src/contexts/ProgressContext.js
+++ src/contexts/ProgressContext.js
@@ -1,8 +1,9 @@
 
 
 
-import React, { createContext, useContext, useEffect, useReducer } from 'react';
+import React, { createContext, useContext, useEffect, useReducer, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { progressEmitter } from '../components/upload/handleUplodadProgress';
 import { useSubscribeToEventsQuery } from '../redux/features/socket/socketApi';
 
 
@@ -11,7 +12,8 @@ const ProgressContext = createContext()
 const processReducer = (state, action) => {
     switch (action.type) {
         case "SET_PROCESS":
-            const { fileName, name, ...rest } = action.payload;
+            const { fileName, name, ...rest } = action.payload || {};
+            if (!fileName) return state;
             return {
                 ...state,
                 [fileName]: {
@@ -30,10 +32,31 @@ export const ProgressProvider = ({ children }) => {
     useSubscribeToEventsQuery();
     const [process, dispatch] = useReducer(processReducer, {});
     const { process: dataProcess } = useSelector((state) => state.socket);
+    const [uploadProgress, setUploadProgress] = useState(null);
+
+    useEffect(() => {
+        const handleProgress = (data) => {
+            setUploadProgress(data);
+        }
+
+        progressEmitter.on('progress', handleProgress);
+
+        return () => {
+            progressEmitter.off('progress', handleProgress);
+        }
+
+    }, [])
+    console.log(uploadProgress, 'uploadProgress');
+
     // Modify your useEffect to dispatch an action with a type and payload
     useEffect(() => {
-        dispatch({ type: "SET_PROCESS", payload: dataProcess });
-    }, [dataProcess]);
+        if (uploadProgress) {
+            dispatch({ type: "SET_PROCESS", payload: uploadProgress });
+        }
+        if (dataProcess) {
+            dispatch({ type: "SET_PROCESS", payload: dataProcess });
+        }
+    }, [dataProcess, uploadProgress]);
 
     return (
         <ProgressContext.Provider value={{ process, dispatch }}>
