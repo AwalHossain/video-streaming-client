b4d2db23c55eef737366d93e227e282be7e80f0c
AwalHossain <waqin21@gmail.com>
2023-11-25 23:01:27 +0000

    feat: progress handler for uploading vidoe 🔁

---
+++ src/components/upload/handleUplodadProgress.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+import EventEmitter from "events";
+import _ from 'lodash';
+import { useState } from "react";
+import REACT_APP_API_URL from "../../utils/apiUrl";
+
+export const progressEmitter = new EventEmitter();
+
+export const useUpload = () => {
+    const [uploading, setUploading] = useState(false);
+    const [data, setData] = useState(null);
+
+    const upload = async (values) => {
+        const formData = new FormData();
+        formData.append('video', values.video);
+        if (values.image) {
+            formData.append('image', values.image);
+        }
+
+        const throttledEmitProgress = _.throttle((progress) => {
+            progressEmitter.emit('progress', progress);
+        }, 3000);
+
+        const config = {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            },
+            onUploadProgress: (progressEvent) => {
+                setUploading(true);
+
+                const { loaded, total } = progressEvent;
+                const percent = Math.floor((loaded * 100) / total);
+
+                throttledEmitProgress({
+                    status: 'processing',
+                    name: "Video Upload",
+                    fileName: values.video.name,
+                    progress: percent,
+                });
+            }
+        };
+
+        try {
+            const response = await axios.post(`${REACT_APP_API_URL}/videos/upload`, formData, config);
+            setData(response.data);
+            setUploading(false);
+            throttledEmitProgress({
+                status: 'processing',
+                name: "Video Upload",
+                fileName: values.video.name,
+                progress: 100,
+            });
+            return response.data;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return { upload, uploading, data };
+};
