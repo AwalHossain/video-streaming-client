cf9916343914b5016d873b459842b816967ecc4f
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-12-04 02:53:30 +0000

    Refactor: VideoGrid and VideoGridItem components

--- src/components/grid/VideoGridItem.js
+++ src/components/grid/VideoGridItem.js
@@ -1,39 +1,48 @@
+import { Box, Skeleton, Typography, useMediaQuery, useTheme } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
+const VideoGridItem = ({ video, isLoading }) => {
+    const { _id, title, author, views, date, duration, fileName, thumbnailUrl, createdAt } =
+        video || {};
 
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
-const VideoGridItem = ({ video }) => {
-    const { _id, title, author, views, date, duration, fileName, thumbnailUrl } =
-        video || {}
     return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-                component="img"
-                alt="green iguana"
-                height="190"
-                image={thumbnailUrl}
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    {title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with over 6,000
-                    species, ranging across all continents except Antarctica
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
-            </CardActions>
-        </Card>
-    )
+        isLoading ? (
+            <Box sx={{ width: isMobile ? '100%' : 210, marginRight: 3, my: 1 }}>
+                <Skeleton variant="rectangular" width={210} height={118} />
+
+
+                <Box sx={{ pt: 0.5 }}>
+                    <Skeleton />
+                    <Skeleton width="60%" />
+                </Box>
+            </Box>
+        ) : (
+            <Link key={_id} to={`/watch/${_id}`} style={{ textDecoration: 'none', color: "black" }}  >
+                <Box sx={{ width: isMobile ? '100%' : 210, marginRight: 3, my: 1 }}>
+                    <img
+                        style={{ width: '100%', height: isMobile ? 'auto' : 118 }}
+                        alt={title}
+                        src={thumbnailUrl}
+                    />
+
+                    <Box sx={{ pr: 2 }}>
+                        <Typography gutterBottom variant="body1">
+                            {title}
+                        </Typography>
+                        <Typography display="block" variant="caption" color="text.secondary">
+                            {"Queen Official"}
+                        </Typography>
+                        <Typography variant="caption" color="text.secondary">
+                            {`${"40M"} • ${createdAt}`}
+                        </Typography>
+                    </Box>
+                </Box>
+            </Link>)
+    );
 }
 
-export default VideoGridItem
+export default VideoGridItem;
