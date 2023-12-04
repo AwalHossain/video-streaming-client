d63681a6ecff042fa40f71705a9cb3ed13c73d2f
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-12-04 02:55:33 +0000

    feat: Add Loading component 🔃

---
+++ src/components/ui/Loading.js
@@ -0,0 +1,19 @@
+import { Box, CircularProgress } from '@mui/material';
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+            }}
+        >
+            <CircularProgress />
+        </Box>
+    );
+}
+
+export default Loading;
