ad58f3cf41b9a25468bfdf4307d36a478c020450
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-12-04 03:04:53 +0000

    Add TAG_TYPES to apiSlice 🏷️

--- src/redux/features/api/apiSlice.js
+++ src/redux/features/api/apiSlice.js
@@ -1,6 +1,7 @@
 import { createApi } from '@reduxjs/toolkit/query/react'
 import axiosBaseQuery from '../../../helpers/axios/axiosBaseQuery'
 import REACT_APP_API_URL from '../../../utils/apiUrl'
+import { TAG_TYPES } from '../tags'
 
 console.log(process.env.REACT_APP_BASE_URL, 'check')
 export const apiSlice = createApi({
@@ -10,5 +11,5 @@ export const apiSlice = createApi({
         baseUrl: REACT_APP_API_URL,
     }),
     endpoints: (builder) => ({}),
-    tagTypes: []
+    tagTypes: [TAG_TYPES.Video, TAG_TYPES.Tag, TAG_TYPES.User, TAG_TYPES.Category]
 })
