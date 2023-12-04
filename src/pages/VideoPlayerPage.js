3bc4b8fa17d658b1b8045788fb5986de2abb11bd
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-12-04 03:03:53 +0000

    Refactor VideoPlayerPage component

--- src/pages/VideoPlayerPage.js
+++ src/pages/VideoPlayerPage.js
@@ -1,79 +1,55 @@
-import { Helmet } from "react-helmet-async";
-// @mui
-import {
-  Container,
-  Stack, Typography
-} from "@mui/material";
-import { styled } from "@mui/material/styles";
+import { Box, Grid } from '@mui/material';
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import Description from '../components/description/Description';
+import Player from '../components/description/Player';
+import Loading from '../components/ui/Loading';
+import RelatedVideo from '../components/ui/list/RelatedVideo';
+import { useGetVideoByIdQuery } from '../redux/features/video/videoApi';
 
+const VideoPlayerPage = () => {
+  const { videoId } = useParams();
+  const { data: videoData, isLoading, isError, error, videoLink, status } = useGetVideoByIdQuery(videoId);
 
-import ReactPlayer from "react-player";
+  const data = videoData?.data;
 
-import React from "react";
-import * as yup from "yup";
+  console.log(data, 'videoData from VideoPlayerPage', status);
 
+  let content;
 
-const StyledContent = styled("div")(({ theme }) => ({
-  maxWidth: 600,
-  margin: "auto",
-  minHeight: "100vh",
-  display: "flex",
-  justifyContent: "center",
-  alignContent: "center",
-  alignItems: "left",
-  flexDirection: "column",
-  padding: theme.spacing(12, 0),
-}));
+  if (isLoading) {
+    content = <Loading />;
+  }
 
-/**
- *  Create a MUI form to save below video properties: 
-    title, description, visibility, 
-    thumbnailUrl, language, recordingDate, 
-    category,
- */
+  if (!isError && !isLoading && !data?._id) {
+    content = <Box sx={{ gridColumn: 'span 12' }}>No video found</Box>;
+  }
 
-const validationSchema = yup.object({
-  title: yup.string().required("Title is required"),
-  description: yup.string().required("Description is required"),
-  visibility: yup.string().required("Visibility is required"),
-  thumbnailUrl: yup.string().required("Thumbnail URL is required"),
-  language: yup.string().required("Language is required"),
-  recordingDate: yup.date().required("Recording date is required"),
-  category: yup.string().required("Category is required"),
-});
+  if (isError) {
+    content = <Box sx={{ gridColumn: 'span 12' }}>{error}</Box>;
+  }
 
-export default function VideoUploadPage() {
-
-
+  if (!isError && data?._id) {
+    content = (
+      <Grid container spacing={2}>
+        <Grid item xs={12} lg={8}>
+          <Player link={videoLink} title={data.thumbnailUrl} />
+          <Description video={data} />
+        </Grid>
+        <Grid item xs={12} lg={4}>
+          <RelatedVideo />
+        </Grid>
+      </Grid>
+    );
+  }
 
   return (
-    <>
-      <Helmet>
-        <title> Video upload</title>
-      </Helmet>
-
-      <>
-        <Container>
-          <StyledContent>
-            <Typography variant="h4" sx={{ mb: 5 }}>
-              Upload video
-            </Typography>
-            <img src="https://mern-video-bucket.sgp1.cdn.digitaloceanspaces.com/yzp2kohp8vd7s2fyexhrvb_1698810470719/2023-10-20_07-47-25_1698811069417.png" />
-            <Stack>
-              <ReactPlayer
-                url="https://mern-video-bucket.sgp1.cdn.digitaloceanspaces.com/u1mw0ljsmyixzcxs172do_1698818161203/2023-10-20_07-47-25_1698818503379_master.m3u8"
-                controls
-                playing
-                width="100%"
-                height="100%"
-                config={{
-                  file: { attributes: { controlsList: "nodownload" } },
-                }}
-              />
-            </Stack>
-          </StyledContent>
-        </Container>
-      </>
-    </>
+    <Box sx={{ pt: 6, pb: 20 }}>
+      <Box sx={{ mx: 'auto', maxWidth: '7xl', px: 2, pb: 20, minHeight: '400px' }}>
+        {content}
+      </Box>
+    </Box>
   );
-}
+};
+
+export default VideoPlayerPage;
