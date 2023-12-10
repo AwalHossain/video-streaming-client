e4df85381d5c5625564f1f8b3aa75a80b5148437
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-12-10 09:47:20 +0000

    refactor: ProtectedRoute to include location state in navigate ✔️

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,18 +1,19 @@
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
 
 const ProtectedRoute = ({ children }) => {
     const { user, loading } = useAuth();
     const navigate = useNavigate();
+    const location = useLocation();
 
     console.log('user', user, 'loading', loading);
 
     useEffect(() => {
         if (!user && !loading) {
-            navigate('/login');
+            navigate('/login', { state: { from: location } });
         }
-    }, [user, loading, navigate]);
+    }, [user, loading, navigate, location]);
 
     if (loading || !user) {
         return <div>Loading...</div>
