e66a00657037a0bf96646e38c5047d03f168999f
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-12-10 22:37:35 +0000

    fix: scrollbar hiding issue solved ✅

--- src/components/ui/list/RelatedVideo.js
+++ src/components/ui/list/RelatedVideo.js
@@ -4,7 +4,9 @@ import RelatedItemList from './RelatedItemList';
 
 const RelatedVideo = () => {
     return (
-        <Box sx={{ overflowY: 'visible', maxHeight: 570, gridColumn: { xs: 'span 1', lg: 'auto' } }}>
+        <Box sx={{
+            gridColumn: { xs: 'span 1', lg: 'auto' }
+        }}>
             {
                 Array.from({ length: 10 }).map((_, index) => (
                     <RelatedItemList key={index} />
