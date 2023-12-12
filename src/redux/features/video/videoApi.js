967a50e4387e8355a51641d8823a15c56d9c1d87
AwalHossain <waqin21@gmail.com>
2023-12-12 23:36:29 +0000

    feat: Add sortBy and sortOrder parameters to getAllVideos query 🔎

--- src/redux/features/video/videoApi.js
+++ src/redux/features/video/videoApi.js
@@ -34,12 +34,14 @@ export const videoApi = apiSlice.injectEndpoints(
                 invalidatesTags: ['Video'],
             }),
             getAllVideos: builder.query({
-                query: ({ searchTerm, tags, page, pageSize }) => {
+                query: ({ searchTerm, tags, page, pageSize, sortBy, sortOrder }) => {
                     const query = new URLSearchParams();
                     if (searchTerm) query.append('searchTerm', searchTerm);
                     if (tags) query.append('tags', tags);
                     if (page) query.append('page', page);
                     if (pageSize) query.append('limit', pageSize);
+                    if (sortBy) query.append('sortBy', sortBy);
+                    if (sortOrder) query.append('sortOrder', sortOrder);
                     const queryString = query.toString() ? `${query.toString()}` : '';
                     return {
                         url: `/videos?${queryString.toString()}`,
