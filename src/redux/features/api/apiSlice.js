65336e63b5c3eeb7b90a040aa4b9fd089cbe489f
AwalHo <182204.te@student.just.edu.bd>
2023-12-12 23:13:17 +0000

    Remove console.log statement ✔️

--- src/redux/features/api/apiSlice.js
+++ src/redux/features/api/apiSlice.js
@@ -3,7 +3,6 @@ import axiosBaseQuery from '../../../helpers/axios/axiosBaseQuery'
 import REACT_APP_API_URL from '../../../utils/apiUrl'
 import { TAG_TYPES } from '../tags'
 
-console.log(process.env.REACT_APP_BASE_URL, 'check')
 export const apiSlice = createApi({
     name: 'api',
     reducerPath: 'api',
