6a9ebd2a8afa5c419e32578c951f9fd08c13f5ef
AwalHossain <waqin21@gmail.com>
2023-12-22 02:17:57 +0000

    Update NAV_WIDTH constant to 250

--- src/layouts/dashboard/nav/index.js
+++ src/layouts/dashboard/nav/index.js
@@ -15,7 +15,7 @@ import navConfig from './config';
 
 // ----------------------------------------------------------------------
 
-const NAV_WIDTH = 280;
+const NAV_WIDTH = 250;
 
 const StyledAccount = styled('div')(({ theme }) => ({
   display: 'flex',
