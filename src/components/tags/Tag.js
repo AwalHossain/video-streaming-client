319ea665e86426e51af495cfb8ecb0ae15a6609c
AwalHossain <waqin21@gmail.com>
2023-12-22 02:16:14 +0000

    refactor: Tag component to use MUI Chip and Redux 🏷️

--- src/components/tags/Tag.js
+++ src/components/tags/Tag.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import Chip from '@mui/material/Chip';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { tagRemoved, tagSelected } from '../../redux/features/filter/filterSlice';
 
-const Tag = () => {
+const Tag = ({ tag }) => {
+    const dispatch = useDispatch();
+
+    const { tags: selectedTags = [] } = useSelector(state => state.filter || {});
+
+    const isSelected = selectedTags.includes(tag?.name);
+
+    const handleSelectTag = () => {
+        console.log('checking tag', isSelected, tag?.name, selectedTags);
+        if (isSelected) {
+            dispatch(tagRemoved(tag?.name))
+        } else {
+            dispatch(tagSelected(tag?.name))
+        }
+    };
+
     return (
-        <div>Tag</div>
-    )
-}
+        <Chip
+            label={tag?.name}
+            color={isSelected ? 'primary' : 'default'}
+            onClick={handleSelectTag}
+            variant="outlined"
+        />
+    );
+};
 
-export default Tag
+export default Tag;
