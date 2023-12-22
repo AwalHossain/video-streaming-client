c77b828bdd03f935239e1c12b32d2a6d410f09e1
AwalHossain <waqin21@gmail.com>
2023-12-22 02:16:49 +0000

    feat: Add Tags component ☑️

--- src/components/tags/Tags.js
+++ src/components/tags/Tags.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Box, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import React from 'react';
+import { VIDEO_TAGS } from '../../utils/constants';
+import Tag from './Tag';
 
 const Tags = () => {
-    return (
-        <div>Tags</div>
-    )
-}
 
-export default Tags
+    const tags = VIDEO_TAGS;
+
+    return (
+        <section>
+            <Grid
+                container
+                // justifyContent="center"
+                alignItems="center"
+                spacing={2}
+            >
+                {tags.map((tag) => (
+                    <Box key={tag.id} m={0.5}>
+                        <Typography variant="body1" fontWeight="fontWeightBold">
+                            <Tag tag={tag} />
+                        </Typography>
+                    </Box>
+                ))}
+            </Grid>
+        </section>
+    );
+};
+
+export default Tags;
