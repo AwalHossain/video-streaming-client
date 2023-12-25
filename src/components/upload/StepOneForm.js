a673df39c201b02cc57dde591032e512f3e1943b
AwalHossain <waqin21@gmail.com>
2023-12-25 02:40:30 +0000

    feat: Add checkbox component and handle tags change ✅🏷️

--- src/components/upload/StepOneForm.js
+++ src/components/upload/StepOneForm.js
@@ -1,26 +1,23 @@
-import { Grid, TextField } from "@mui/material";
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import { Autocomplete, Grid, TextField } from "@mui/material";
+import Checkbox from '@mui/material/Checkbox';
+
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
 const StepOne = ({ formik }) => {
     console.log('title');
+
+    // Sample tags. Replace this with your actual tags.
+    const tags = ['Tag1', 'Tag2', 'Tag3', 'Tag4', 'Tag5', "Tag6", "tag00"];
+
+    const handleTagsChange = (event, value) => {
+        formik.setFieldValue('tags', value);
+    };
     return (
         <>
-            {/* <Grid item xs={12} md={6}>
-                <TextField
-                    label="Video file name"
-                    value={formik.values.videoFile?.name}
-                    error={Boolean(formik.errors?.videoFile)}
-                    helperText={formik.errors?.videoFile}
-                    fullWidth
-                />
-                {
-                    formik.errors?.videoFile && (
-                        <div style={{ color: 'red' }}>
-                            {formik.errors?.videoFile}
-                        </div>
-                    )
-                }
-            </Grid> */}
-            <Grid item xs={12}>
+            <Grid item xs={12} spacing={5}>
                 <TextField
                     id="title"
                     name="title"
@@ -32,7 +29,8 @@ const StepOne = ({ formik }) => {
                     fullWidth
                 />
             </Grid>
-            <Grid item xs={12}>
+
+            <Grid item xs={12} spacing={5}>
                 <TextField
                     id="description"
                     name="description"
@@ -48,6 +46,32 @@ const StepOne = ({ formik }) => {
                         formik.touched.description && formik.errors.description
                     }
                     fullWidth
+                />
+            </Grid>
+            <Grid item xs={12} spacing={5} >
+                <Autocomplete
+                    multiple
+                    id="checkboxes-tags-demo"
+                    options={tags}
+                    disableCloseOnSelect
+                    getOptionLabel={(option) => option}
+                    onChange={handleTagsChange}
+                    value={formik.values.tags}
+                    renderOption={(props, option, { selected }) => (
+                        <li {...props}>
+                            <Checkbox
+                                icon={icon}
+                                checkedIcon={checkedIcon}
+                                style={{ marginRight: 8 }}
+                                checked={selected}
+                            />
+                            {option}
+                        </li>
+                    )}
+                    // style={{ width: 500 }}
+                    renderInput={(params) => (
+                        <TextField {...params} label="Checkboxes" placeholder="Favorites" />
+                    )}
                 />
             </Grid>
         </>
