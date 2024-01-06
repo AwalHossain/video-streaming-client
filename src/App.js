34f3f0e95dbddc3425ae8ed740a5c7d86a1a9f3b
AwalHossain <waqin21@gmail.com>
2024-01-06 06:24:24 +0000

    feat: Add Redux socket subscription to App component

--- src/App.js
+++ src/App.js
@@ -8,16 +8,20 @@ import Router from "./routes";
 import ThemeProvider from "./theme";
 
 // components
+import { useSelector } from "react-redux";
 import ProgressModals from "./components/modal/ProgressModals";
 import ScrollToTop from "./components/scroll-to-top";
 import Loading from "./components/ui/Loading";
 import { ProgressProvider } from "./contexts/ProgressContext";
 import useAuth from "./hooks/useAuth";
+import { useSubscribeToEventsQuery } from "./redux/features/socket/socketApi";
 import NotificationBar from "./utils/NotificationBar";
 
 export default function App() {
   const { loading } = useAuth();
-
+  const user = useSelector((state) => state.auth.user);
+  const userId = user ? user._id : null;
+  useSubscribeToEventsQuery({ userId });
   if (loading) {
     return (
       <Box
