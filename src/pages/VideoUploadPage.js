0bf2aa5b787ea2fc5c35322e15050beecb76d472
AwalHossain <waqin21@gmail.com>
2024-01-06 06:26:58 +0000

    Remove unused imports and variables in VideoUploadPage.js

--- src/pages/VideoUploadPage.js
+++ src/pages/VideoUploadPage.js
@@ -1,15 +1,11 @@
 import { Button } from '@mui/material'; // Import Button from Material-UI
 import { Box } from '@mui/system'; // Import Box from Material-UI
 import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { useSubscribeToEventsQuery } from '../redux/features/socket/socketApi';
 import { UploadModal } from './uploadModal';
 
 export default function VideoUploadPage() {
-  const { _id: userId } = useSelector((state) => state.auth.user);
   const [open, setOpen] = React.useState(true);
-  useSubscribeToEventsQuery({ userId });
 
   const navigate = useNavigate();
   const location = useLocation();
