d7e7b334302d16b7fbfb2a4bd1175b21dd065fc9
AwalHossain <waqin21@gmail.com>
2024-01-14 09:41:53 +0000

    refactor: protectedRoute component to use Navigate instead of react-router-dom

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,26 +1,10 @@
-import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
-    const { user, userLoggedIn } = useSelector(state => state.auth)
-    const navigate = useNavigate();
-    const location = useLocation();
-    const [isLoading, setIsLoading] = useState(true);
+    const { user } = useSelector(state => state.auth); // replace with your auth context
 
-    useEffect(() => {
-        if (!user && !userLoggedIn) {
-            navigate('/login', { state: { from: location } });
-        }
-        setIsLoading(false);
-    }, [user, userLoggedIn, navigate, location]);
-
-
-    if (isLoading) {
-        return <div>loading....</div>; // or return a loading spinner
-    }
-
-    return children;
+    return user ? children : <Navigate to="/login" />;
 }
 
-export default ProtectedRoute;
+export default ProtectedRoute; 
