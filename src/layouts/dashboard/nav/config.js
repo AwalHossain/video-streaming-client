52435fe7b803ba16ca786c65307de5a08b14dbf3
AwalHossain <waqin21@gmail.com>
2024-01-14 09:44:31 +0000

    Refactor navConfig function to dynamically exclude login option based on user

--- src/layouts/dashboard/nav/config.js
+++ src/layouts/dashboard/nav/config.js
@@ -10,42 +10,40 @@ const icon = (name) => (
   />
 );
 
-const navConfig = [
-  {
-    title: "dashboard",
-    path: "/dashboard/app",
-    icon: icon("dashboard"),
-  },
-  {
-    title: "Content",
-    path: "/dashboard/user",
-    icon: icon("videos"),
-  },
-  {
-    title: "product",
-    path: "/dashboard/products",
-    icon: icon("ic_cart"),
-  },
-  {
-    title: "blog",
-    path: "/dashboard/blog",
-    icon: icon("ic_blog"),
-  },
-  {
-    title: "Video upload",
-    path: "/dashboard/video-upload",
-    icon: icon("video-upload-fill"),
-  },
-  {
-    title: "login",
-    path: "/login",
-    icon: icon("ic_lock"),
-  },
-  {
-    title: "Not found",
-    path: "/404",
-    icon: icon("ic_disabled"),
-  },
-];
+const navConfig = (user) => {
+  let config = [
+    {
+      title: "dashboard",
+      path: "/dashboard/app",
+      icon: icon("dashboard"),
+    },
+    {
+      title: "Content",
+      path: "/dashboard/content",
+      icon: icon("videos"),
+    },
+    {
+      title: "Video upload",
+      path: "/dashboard/video-upload",
+      icon: icon("video-upload-fill"),
+    },
+    {
+      title: "login",
+      path: "/login",
+      icon: icon("ic_lock"),
+    },
+    {
+      title: "Not found",
+      path: "/404",
+      icon: icon("ic_disabled"),
+    },
+  ];
+
+  if (user) {
+    config = config.filter(item => item.title !== "login");
+  }
+
+  return config;
+};
 
 export default navConfig;
