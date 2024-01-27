44e2cb14aabe217e174712960dc82faffe2b30d4
AwalHossain <waqin21@gmail.com>
2024-01-27 10:17:54 +0000

    Add socket connection and user information to DashboardAppPage

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -2,14 +2,20 @@ import { Helmet } from 'react-helmet-async';
 // @mui
 import { Container } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
+import { useDispatch, useSelector } from 'react-redux';
 import VideoGrid from '../components/grid/VideoGrid';
 import Tags from '../components/tags/Tags';
+import { connectSocket } from '../redux/features/socket/socketApi';
 
 
 export default function DashboardAppPage() {
   const theme = useTheme();
-
-  console.log('data from DashboardAppPage');
+  const user = useSelector((state) => state.auth.user);
+  const userId = user ? user._id : null;
+  const dispatch = useDispatch();
+  console.log('Before connectSocket', userId, dispatch);
+  const data = connectSocket(userId, dispatch);
+  console.log('After connectSocket', data);
   return (
     <>
       <Helmet>
