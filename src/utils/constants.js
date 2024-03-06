bae709823168fe64ada58e1b89fa65f7571214b7
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2024-03-06 15:31:21 +0000

    Update NOTIFY_EVENTS and VIDEO_TAGS constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,94 +1,92 @@
-
-
 export const NOTIFY_EVENTS = {
-    NOTIFY_VIDEO_HLS_CONVERTED: "notify.video.hls.converted",
-    NOTIFY_VIDEO_WATERMARKED: "notify.video.watermarked",
-    NOTIFY_UPLOAD_PROGRESS: "notify.upload.progress",
-    NOTIFY_VIDEO_UPLOADED: "notify.video.uploaded",
-    NOTIFY_VIDEO_PROCESSING: "notify.video.processing",
-    NOTIFY_VIDEO_PROCESSED: "notify.video.processed",
-    NOTIFY_EVENTS_VIDEO_BIT_RATE_PROCESSED: "notify.video.bit-rate.processed",
-    NOTIFY_EVENTS_VIDEO_BIT_RATE_PROCESSING: "notify.video.bit-rate.processing",
-    NOTIFY_VIDEO_THUMBNAIL_GENERATED: "notify.video.thumbnail-generated",
-    NOTIFY_VIDEO_METADATA_SAVED: "notify.video.metadata.saved",
-    NOTIFY_EVENTS_FAILED: "notify.failed",
-    NOTIFY_AWS_S3_UPLOAD_FAILED: "notify.aws.s3.upload.failed",
-    NOTIFY_AWS_S3_UPLOAD_PROGRESS: "notify.aws.s3.upload.progress",
-    NOTIFY_AWS_S3_UPLOAD_COMPLETED: "notify.aws.s3.upload.completed",
-    NOTIFY_VIDEO_PUBLISHED: "notify.video.published",
-    NOTIFY_VIDEO_INITIAL_DB_INFO: "notify.video.initial.db.info",
+  NOTIFY_VIDEO_HLS_CONVERTED: "notify.video.hls.converted",
+  NOTIFY_VIDEO_WATERMARKED: "notify.video.watermarked",
+  NOTIFY_UPLOAD_PROGRESS: "notify.upload.progress",
+  NOTIFY_VIDEO_DOWNLOADING: "notify.video.downloading",
+  NOTIFY_VIDEO_DOWNLOADED: "notify.video.downloaded",
+  NOTIFT_VIDEO_UPLOADING_BUCKET: "notify.video.uploading.bucket",
+  NOTIFY_VIDEO_UPLOADED: "notify.video.uploaded",
+  NOTIFY_VIDEO_PROCESSING: "notify.video.processing",
+  NOTIFY_VIDEO_PROCESSED: "notify.video.processed",
+  NOTIFY_EVENTS_VIDEO_BIT_RATE_PROCESSED: "notify.video.bit-rate.processed",
+  NOTIFY_EVENTS_VIDEO_BIT_RATE_PROCESSING: "notify.video.bit-rate.processing",
+  NOTIFY_VIDEO_THUMBNAIL_GENERATED: "notify.video.thumbnail-generated",
+  NOTIFY_VIDEO_METADATA_SAVED: "notify.video.metadata.saved",
+  NOTIFY_EVENTS_FAILED: "notify.failed",
+  NOTIFY_AWS_S3_UPLOAD_FAILED: "notify.aws.s3.upload.failed",
+  NOTIFY_AWS_S3_UPLOAD_PROGRESS: "notify.aws.s3.upload.progress",
+  NOTIFY_AWS_S3_UPLOAD_COMPLETED: "notify.aws.s3.upload.completed",
+  NOTIFY_VIDEO_PUBLISHED: "notify.video.published",
+  NOTIFY_VIDEO_INITIAL_DB_INFO: "notify.video.initial.db.info",
 };
 
-
-
-
 export const VIDEO_TAGS = [
-    {
-        name: "Comedy",
-        id: 1
-    },
-    {
-        name: "Fitness",
-        id: 2
-    },
-    {
-        name: "Food",
-        id: 3
-    },
-    {
-        name: "Gaming",
-        id: 4
-    },
-    {
-        name: "Music",
-        id: 5
-    },
-    {
-        name: "Sports",
-        id: 6
-    },
-    {
-        name: "Technology",
-        id: 7
-    },
-    {
-        name: "Travel",
-        id: 8
-    },
-    {
-        name: "Vlog",
-        id: 9
-    },
-    {
-        name: "Education",
-        id: 10
-    },
-    {
-        name: "Motivational",
-        id: 11
-    },
-    {
-        name: "Lifestyle",
-        id: 12
-    },
-    {
-        name: "Fashion",
-        id: 13
-    },
-    {
-        name: "Science",
-        id: 14
-    },
-    {
-        name: "Health",
-        id: 15
-    },
-    {
-        name: "Business",
-        id: 16
-    },
-    {
-        name: "Entertainment",
-        id: 17
-    },
+  {
+    name: "Comedy",
+    id: 1,
+  },
+  {
+    name: "Fitness",
+    id: 2,
+  },
+  {
+    name: "Food",
+    id: 3,
+  },
+  {
+    name: "Gaming",
+    id: 4,
+  },
+  {
+    name: "Music",
+    id: 5,
+  },
+  {
+    name: "Sports",
+    id: 6,
+  },
+  {
+    name: "Technology",
+    id: 7,
+  },
+  {
+    name: "Travel",
+    id: 8,
+  },
+  {
+    name: "Vlog",
+    id: 9,
+  },
+  {
+    name: "Education",
+    id: 10,
+  },
+  {
+    name: "Motivational",
+    id: 11,
+  },
+  {
+    name: "Lifestyle",
+    id: 12,
+  },
+  {
+    name: "Fashion",
+    id: 13,
+  },
+  {
+    name: "Science",
+    id: 14,
+  },
+  {
+    name: "Health",
+    id: 15,
+  },
+  {
+    name: "Business",
+    id: 16,
+  },
+  {
+    name: "Entertainment",
+    id: 17,
+  },
 ];
