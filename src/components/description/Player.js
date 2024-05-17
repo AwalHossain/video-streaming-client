9291a2a3addb8ed6bea5cba90c58faf228112cac
AwalHo <182204.te@student.just.edu.bd>
2024-05-17 14:49:31 +0000

    chore: Refactor Player component for improved readability

--- src/components/description/Player.js
+++ src/components/description/Player.js
@@ -1,66 +1,67 @@
-import './player.css';
+import "./player.css";
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from "react";
 
 import {
-    isHLSProvider,
-    MediaPlayer,
-    MediaProvider,
-    Poster
-} from '@vidstack/react';
-import { defaultLayoutIcons, DefaultVideoLayout } from '@vidstack/react/player/layouts/default';
+  isHLSProvider,
+  MediaPlayer,
+  MediaProvider,
+  Poster,
+} from "@vidstack/react";
+import {
+  defaultLayoutIcons,
+  DefaultVideoLayout,
+} from "@vidstack/react/player/layouts/default";
 
 export default function Player({ link, thumbnailUrl }) {
-    let player = useRef(null);
-    const muxPlaybackId = 'VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU';
-    const src = `https://stream.mux.com/${muxPlaybackId}.m3u8`;
+  let player = useRef(null);
 
-    useEffect(() => {
-        // Subscribe to state updates.
-        return player.current.subscribe(({ paused, viewType }) => {
-            // console.log('is paused?', '->', paused);
-            // console.log('is audio view?', '->', viewType === 'audio');
-        });
-    }, []);
+  useEffect(() => {
+    // Subscribe to state updates.
+    return player.current.subscribe(({ paused, viewType }) => {
+      // console.log('is paused?', '->', paused);
+      // console.log('is audio view?', '->', viewType === 'audio');
+    });
+  }, []);
 
-    function onProviderChange(provider, nativeEvent) {
-        // We can configure provider's here.
-        if (isHLSProvider(provider)) {
-            provider.config = {};
-        }
+  function onProviderChange(provider, nativeEvent) {
+    // We can configure provider's here.
+    if (isHLSProvider(provider)) {
+      provider.config = {};
     }
+  }
 
-    // We can listen for the `can-play` event to be notified when the player is ready.
-    function onCanPlay(detail, nativeEvent) {
-        // ...
-    }
+  // We can listen for the `can-play` event to be notified when the player is ready.
+  function onCanPlay(detail, nativeEvent) {
+    // ...
+  }
 
-    return (
-        <>
-            <MediaPlayer
-                className="player"
-                title="Sprite Fight"
-                src={link}
-                crossorigin
-                playsinline
-                onProviderChange={onProviderChange}
-                onCanPlay={onCanPlay}
-                ref={player}
-            >
-                <MediaProvider>
-                    <Poster
-                        className="vds-poster"
-                        src={thumbnailUrl}
-                        alt="Girl walks into campfire with gnomes surrounding her friend ready for their next meal!"
-                    />
-                </MediaProvider>
+  return (
+    <>
+      <MediaPlayer
+        className="player"
+        title="Sprite Fight"
+        src={link}
+        crossorigin
+        playsinline
+        onProviderChange={onProviderChange}
+        onCanPlay={onCanPlay}
+        ref={player}
+      >
+        <MediaProvider>
+          <Poster
+            className="vds-poster"
+            src={thumbnailUrl}
+            alt="Girl walks into campfire with gnomes surrounding her friend ready for their next meal!"
+          />
+        </MediaProvider>
 
-                {/* Layouts */}
-                <DefaultVideoLayout
-                    icons={defaultLayoutIcons}
-                    thumbnails={{ thumbnailUrl }}
-                />
-            </MediaPlayer>
-        </>
-    );
+        {/* Layouts */}
+        <DefaultVideoLayout
+          icons={defaultLayoutIcons}
+          thumbnails={{ thumbnailUrl }}
+        />
+      </MediaPlayer>
+    </>
+  );
 }
