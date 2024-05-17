b637847a85964e399d66a44059e265aec0f9dccb
AwalHo <182204.te@student.just.edu.bd>
2024-05-17 14:50:27 +0000

    chore: Refactor Player and Description components for improved readability

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -1,21 +1,20 @@
-import { Helmet } from 'react-helmet-async';
+import { Helmet } from "react-helmet-async";
 // @mui
-import { Container } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
-import { useDispatch, useSelector } from 'react-redux';
-import VideoGrid from '../components/grid/VideoGrid';
-import Tags from '../components/tags/Tags';
-import { connectSocket } from '../redux/features/socket/socketApi';
-
+import { Container } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import { useDispatch, useSelector } from "react-redux";
+import VideoGrid from "../components/grid/VideoGrid";
+import Tags from "../components/tags/Tags";
+import { connectSocket } from "../redux/features/socket/socketApi";
 
 export default function DashboardAppPage() {
   const theme = useTheme();
   const user = useSelector((state) => state.auth.user);
   const userId = user ? user._id : null;
   const dispatch = useDispatch();
-  console.log('Before connectSocket', userId, dispatch);
+  console.log("Before connectSocket", userId, dispatch);
   const data = connectSocket(userId, dispatch);
-  console.log('After connectSocket', data);
+  console.log("After connectSocket", data);
   return (
     <>
       <Helmet>
@@ -23,6 +22,7 @@ export default function DashboardAppPage() {
       </Helmet>
 
       <Container maxWidth="xl">
+        <p>Come on man</p>
         <Tags />
         <VideoGrid />
       </Container>
