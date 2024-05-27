b2a0a9596d090ca9f226888b0be2ba66dc2c4b00
AwalHossain <waqin21@gmail.com>
2024-05-27 11:54:52 +0000

    refactor: Remove unused theme variable in DashboardAppPage.jsx

--- src/pages/DashboardAppPage.jsx
+++ src/pages/DashboardAppPage.jsx
@@ -1,14 +1,12 @@
 import { Helmet } from "react-helmet-async";
 // @mui
 import { Container } from "@mui/material";
-import { useTheme } from "@mui/material/styles";
 import { useDispatch, useSelector } from "react-redux";
 import VideoGrid from "../components/grid/VideoGrid";
 import Tags from "../components/tags/Tags";
 import { connectSocket } from "../redux/features/socket/socketApi";
 
 export default function DashboardAppPage() {
-  const theme = useTheme();
   const user = useSelector((state) => state.auth.user);
   const userId = user ? user._id : null;
   const dispatch = useDispatch();
@@ -22,7 +20,6 @@ export default function DashboardAppPage() {
       </Helmet>
 
       <Container maxWidth="xl">
-        <p>Come on man</p>
         <Tags />
         <VideoGrid />
       </Container>
