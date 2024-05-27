85a7a2ffb03d17279d312e8d5d193634f0e4bef5
AwalHossain <waqin21@gmail.com>
2024-05-27 11:55:19 +0000

    refactor: Remove unused import statement in ProfilePage.jsx

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -4,7 +4,7 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import React from 'react';
+
 
 const ProfilePage = ({ open, onClose }) => {
     // const [open, setOpen] = useState(true);
