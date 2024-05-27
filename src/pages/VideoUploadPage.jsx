6e30581bd00b4dcc3aeb540be8bc913ad9454a89
AwalHossain <waqin21@gmail.com>
2024-05-27 11:57:59 +0000

    refactor: Update VideoUploadPage component to use VideoUploadModal

--- src/pages/VideoUploadPage.jsx
+++ src/pages/VideoUploadPage.jsx
@@ -1,19 +1,13 @@
 import { Button } from '@mui/material'; // Import Button from Material-UI
 import { Box } from '@mui/system'; // Import Box from Material-UI
-import React, { useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { UploadModal } from './uploadModal';
+import React from 'react';
+import { VideoUploadModal } from './VideoUploadModal';
 
 export default function VideoUploadPage() {
   const [open, setOpen] = React.useState(true);
 
-  const navigate = useNavigate();
-  const location = useLocation();
-  const [prevLocation, setPrevLocation] = useState();
-
   const handleClose = () => {
     setOpen(false);
-    console.log("prevLocation", prevLocation);
   };
 
   return (
@@ -26,7 +20,7 @@ export default function VideoUploadPage() {
       <Button variant="contained" color="primary" onClick={() => setOpen(true)}>
         Video Upload
       </Button>
-      <UploadModal open={open} onClose={handleClose} />
+      <VideoUploadModal open={open} onClose={handleClose} />
     </Box>
   );
-};
+}
