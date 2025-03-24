d4750e25b5206a6784f1e23128ddd49394cc1382
Awalhossain <waqin21@gmail.com>
2025-03-24 15:53:57 +0600

    refactor: main file code changed

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { store } from "./redux/store";
+
+// Import your CSS here
+import "./index.css";
+
+// Create root and render app
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <HelmetProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </HelmetProvider>
+    </Provider>
+  </React.StrictMode>
+); 
